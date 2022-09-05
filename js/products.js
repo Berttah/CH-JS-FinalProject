f662f58ec46b102976e3aa7dab89a4f1ed388b9d
Bertto <nachebertah@gmail.com>
2022-09-05 18:52:03 -0300

    1ra Entrega PF

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 // this constructor is used to make the items on the store
 class Product {
-    constructor(name,id,price,quantity) {
-        this.name = name
+    constructor(pName,id,price,quantity) {
+        this.pName = pName
         this.id = id
         this.price = price
         this.quantity = quantity
@@ -11,10 +11,10 @@ class Product {
         console.log(orderQty)
         // this cycle checks for an initial stock and if the user order is valid
         if (this.quantity > 0 && this.quantity - orderQty >= 0) {
-            console.log(`Is there stock of ${this.name}? Yes, ${this.quantity}`)
+            console.log(`Is there stock of ${this.pName}? Yes, ${this.quantity}`)
             return true
         } else {
-            console.log(`There is no stock of ${this.name}`)
+            console.log(`There is no stock of ${this.pName}`)
             return false
         }       
     }
