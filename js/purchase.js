1ba5c8862ec1a56ede6fafd24adb0db248abe6e5
Bertto <nachebertah@gmail.com>
2022-09-24 11:47:48 -0300

    Optimization

--- js/purchase.js
+++ js/purchase.js
@@ -1,10 +1,8 @@
 // this creates the cards for the purchased items
 function purchaseDisplay() {
     for (let i=0;i<localStorage.length;i++){
-    
             const key = localStorage.key(i);
             quantity = localStorage.getItem(key)
-    
             if (!isNaN(key)){
                 let cardPurchase = document.createElement('div');
                 cardPurchase.classList.add('cartCard')
@@ -16,33 +14,24 @@ function purchaseDisplay() {
                                                 </div>
                                             <p>$${(inventory[key-1].price)*quantity}</p>`;
                 document.getElementById('cACPurchase').appendChild(cardPurchase)
-    
             }
     }
 }
-
 
 // These are to display a message based by the delivery option
 let selectedShipping = localStorage.getItem('Shipping Price')
 let deliveryMsg = document.getElementById('deliveryMsg')
 
-if (selectedShipping == 0) {
-    deliveryMsg.innerText = 'Since you selected for no delivery, your purchase will be at the local waiting for you to pick up.'
-} else if (selectedShipping == 300) {
-    deliveryMsg.innerText = 'Since you selected for a close delivery, your purchase will arrive to the area between 1 to 2 working days.'
-} else if (selectedShipping == 800) {
-    deliveryMsg.innerText = 'Since you selected for a near delivery, your purchase will arrive to the area between 5 to 10 working days.'
-} else if (selectedShipping == 1200) {
-    deliveryMsg.innerText = 'Since you selected for a far delivery, your purchase will arrive to the area between 10 to 15 working days.'
-}
-
+selectedShipping == 0 && (deliveryMsg.innerText = 'Since you selected for no delivery, your purchase will be at the local waiting for you to pick up.')
+selectedShipping == 300 && (deliveryMsg.innerText = 'Since you selected for a close delivery, your purchase will arrive to the area between 1 to 2 working days.')
+selectedShipping == 800 && (deliveryMsg.innerText = 'Since you selected for a near delivery, your purchase will arrive to the area between 5 to 10 working days.')
+selectedShipping == 1200 && (deliveryMsg.innerText = 'Since you selected for a far delivery, your purchase will arrive to the area between 10 to 15 working days.')
 
 // These are to display a message based in the user data
 let userDataPurchase = JSON.parse(localStorage.getItem('User Data'))
-console.log(userDataPurchase)
+    console.log(userDataPurchase)
 let userMsg = document.getElementById('userMsg')
 userMsg.innerText = `We thank you ${userDataPurchase['first_name']} ${userDataPurchase['last_name']} for your purchase. `
-
 
 // This one displays the final price
 let finalPricePurchase = localStorage.getItem('Final Price')
