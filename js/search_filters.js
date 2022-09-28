44ff575e7e8a9f16d0b5973a395970a4128918b9
Bertto <nachebertah@gmail.com>
2022-09-28 11:20:27 -0300

    Libraries

--- js/search_filters.js
+++ js/search_filters.js
@@ -3,9 +3,31 @@ document.getElementById('searchInput').addEventListener('keypress', function(e) 
     // check if the element is an input element and the key is enter
     if (e.target.nodeName === "INPUT" && e.key === 'Enter') {
         let result = inventory.find(item => item.pName === e.target.value)
+        let text = ''
         result !== undefined 
-            ? alert(`We have ${result.quantity} units of ${result.pName}`) 
-            : alert("We don't have that one, ask for one that is displayed instead")
+            ? text = `We have ${result.quantity} units of ${result.pName}`
+            : text = "We don't have that one, ask for one that is displayed instead"
+
+        Swal.fire({
+            html: ` 
+                    <p class= "swal2-title"> Search Result </p>
+                    <div class= "swalHTML">
+                        <img class="swalImg" src="././assets/open-book.png" alt="">
+                        <p class="swalText">${text}</p>
+                    </div>
+                  `,
+            showConfirmButton: false,
+            width: '600px',
+            padding: '0em',
+            position: 'top',
+            background: '#FFFFF9',
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
     }
   }
 );
